1de3d639eaa08ccbed6dcd25eb89152944d9f78e
김경현 <ryan4078@gmail.com>
2019-08-30 17:30:18 +0900

    게시판 bean파일 만들었습니다.

---
+++ Travel/src/main/java/com/itwill/domain/BoardBean.java
@@ -0,0 +1,165 @@
+package com.itwill.domain;
+
+import java.security.Timestamp;
+
+public class BoardBean {
+	private int seq;
+	private String board_id;
+	private String subject;
+	private String content;
+	private String country;
+	private String file_use;
+	private String notice_use;
+	private String secret_use;
+	private int readcount;
+	private int like;
+	private int hate;
+	private int recommend;
+	private String etc1;
+	private String etc2;
+	private String etc3;
+	private String etc4;
+	private String regi_id;
+	private Timestamp regi_date;
+	private String regi_ip_addr;
+	private String modifier;
+	private Timestamp modifier_date;
+	private String modifier_ip_addr;
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getBoard_id() {
+		return board_id;
+	}
+	public void setBoard_id(String board_id) {
+		this.board_id = board_id;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getFile_use() {
+		return file_use;
+	}
+	public void setFile_use(String file_use) {
+		this.file_use = file_use;
+	}
+	public String getNotice_use() {
+		return notice_use;
+	}
+	public void setNotice_use(String notice_use) {
+		this.notice_use = notice_use;
+	}
+	public String getSecret_use() {
+		return secret_use;
+	}
+	public void setSecret_use(String secret_use) {
+		this.secret_use = secret_use;
+	}
+	public int getReadcount() {
+		return readcount;
+	}
+	public void setReadcount(int readcount) {
+		this.readcount = readcount;
+	}
+	public int getLike() {
+		return like;
+	}
+	public void setLike(int like) {
+		this.like = like;
+	}
+	public int getHate() {
+		return hate;
+	}
+	public void setHate(int hate) {
+		this.hate = hate;
+	}
+	public int getRecommend() {
+		return recommend;
+	}
+	public void setRecommend(int recommend) {
+		this.recommend = recommend;
+	}
+	public String getEtc1() {
+		return etc1;
+	}
+	public void setEtc1(String etc1) {
+		this.etc1 = etc1;
+	}
+	public String getEtc2() {
+		return etc2;
+	}
+	public void setEtc2(String etc2) {
+		this.etc2 = etc2;
+	}
+	public String getEtc3() {
+		return etc3;
+	}
+	public void setEtc3(String etc3) {
+		this.etc3 = etc3;
+	}
+	public String getEtc4() {
+		return etc4;
+	}
+	public void setEtc4(String etc4) {
+		this.etc4 = etc4;
+	}
+	public String getRegi_id() {
+		return regi_id;
+	}
+	public void setRegi_id(String regi_id) {
+		this.regi_id = regi_id;
+	}
+	public Timestamp getRegi_date() {
+		return regi_date;
+	}
+	public void setRegi_date(Timestamp regi_date) {
+		this.regi_date = regi_date;
+	}
+	public String getRegi_ip_addr() {
+		return regi_ip_addr;
+	}
+	public void setRegi_ip_addr(String regi_ip_addr) {
+		this.regi_ip_addr = regi_ip_addr;
+	}
+	public String getModifier() {
+		return modifier;
+	}
+	public void setModifier(String modifier) {
+		this.modifier = modifier;
+	}
+	public Timestamp getModifier_date() {
+		return modifier_date;
+	}
+	public void setModifier_date(Timestamp modifier_date) {
+		this.modifier_date = modifier_date;
+	}
+	public String getModifier_ip_addr() {
+		return modifier_ip_addr;
+	}
+	public void setModifier_ip_addr(String modifier_ip_addr) {
+		this.modifier_ip_addr = modifier_ip_addr;
+	}
+	
+	
+	
+	
+	
+}
---
+++ Travel/src/main/java/com/itwill/domain/BoardSettingBean.java
@@ -0,0 +1,148 @@
+package com.itwill.domain;
+
+import java.security.Timestamp;
+
+public class BoardSettingBean {
+	private int seq;
+	private String board_id;
+	private String board_name;
+	private String board_type;
+	private String writing_authority;
+	private String modification_authority;
+	private String delete_authority;
+	private String read_authority;
+	private int page_size;
+	private int pages_size;
+	private String notification_use;
+	private String file_use;
+	private String re_write_use;
+	private String comments_use;
+	private String regi_id;
+	private Timestamp regi_date;
+	private String regi_ip_addr;
+	private String modifier_id;
+	private Timestamp modifier_date;
+	private String modifier_ip_addr;
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getBoard_id() {
+		return board_id;
+	}
+	public void setBoard_id(String board_id) {
+		this.board_id = board_id;
+	}
+	public String getBoard_name() {
+		return board_name;
+	}
+	public void setBoard_name(String board_name) {
+		this.board_name = board_name;
+	}
+	public String getBoard_type() {
+		return board_type;
+	}
+	public void setBoard_type(String board_type) {
+		this.board_type = board_type;
+	}
+	public String getWriting_authority() {
+		return writing_authority;
+	}
+	public void setWriting_authority(String writing_authority) {
+		this.writing_authority = writing_authority;
+	}
+	public String getModification_authority() {
+		return modification_authority;
+	}
+	public void setModification_authority(String modification_authority) {
+		this.modification_authority = modification_authority;
+	}
+	public String getDelete_authority() {
+		return delete_authority;
+	}
+	public void setDelete_authority(String delete_authority) {
+		this.delete_authority = delete_authority;
+	}
+	public String getRead_authority() {
+		return read_authority;
+	}
+	public void setRead_authority(String read_authority) {
+		this.read_authority = read_authority;
+	}
+	public int getPage_size() {
+		return page_size;
+	}
+	public void setPage_size(int page_size) {
+		this.page_size = page_size;
+	}
+	public int getPages_size() {
+		return pages_size;
+	}
+	public void setPages_size(int pages_size) {
+		this.pages_size = pages_size;
+	}
+	public String getNotification_use() {
+		return notification_use;
+	}
+	public void setNotification_use(String notification_use) {
+		this.notification_use = notification_use;
+	}
+	public String getFile_use() {
+		return file_use;
+	}
+	public void setFile_use(String file_use) {
+		this.file_use = file_use;
+	}
+	public String getRe_write_use() {
+		return re_write_use;
+	}
+	public void setRe_write_use(String re_write_use) {
+		this.re_write_use = re_write_use;
+	}
+	public String getComments_use() {
+		return comments_use;
+	}
+	public void setComments_use(String comments_use) {
+		this.comments_use = comments_use;
+	}
+	public String getRegi_id() {
+		return regi_id;
+	}
+	public void setRegi_id(String regi_id) {
+		this.regi_id = regi_id;
+	}
+	public Timestamp getRegi_date() {
+		return regi_date;
+	}
+	public void setRegi_date(Timestamp regi_date) {
+		this.regi_date = regi_date;
+	}
+	public String getRegi_ip_addr() {
+		return regi_ip_addr;
+	}
+	public void setRegi_ip_addr(String regi_ip_addr) {
+		this.regi_ip_addr = regi_ip_addr;
+	}
+	public String getModifier_id() {
+		return modifier_id;
+	}
+	public void setModifier_id(String modifier_id) {
+		this.modifier_id = modifier_id;
+	}
+	public Timestamp getModifier_date() {
+		return modifier_date;
+	}
+	public void setModifier_date(Timestamp modifier_date) {
+		this.modifier_date = modifier_date;
+	}
+	public String getModifier_ip_addr() {
+		return modifier_ip_addr;
+	}
+	public void setModifier_ip_addr(String modifier_ip_addr) {
+		this.modifier_ip_addr = modifier_ip_addr;
+	}
+	
+	
+}
